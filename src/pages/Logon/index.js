bdaef2df66b9792df42a7d87ed119216c9d164cf
Gabriel Davi <gdavi.gd@gmail.com>
2020-04-22 01:22:42 -0300

    Conected front to backend

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
 import { FiLogIn } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import api from "../../services/api";
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const respose = await api.post("/session", { id });
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", respose.data.name);
+      history.push("/profile");
+    } catch (error) {
+      alert("Falha no login, tente novamente");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be the hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="seu Id" />
+          <input
+            type="text"
+            placeholder="seu Id"
+            onChange={(event) => setId(event.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import logoImg from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  const history = useHistory();
+  async function handleRegister(event) {
+    event.preventDefault();
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const respose = await api.post("/ongs", data);
+      history.push("/");
+      alert(`Seu ID de acesso ${respose.data.id}`);
+    } catch (error) {
+      alert(`Erro no cadastro, tente novamente`);
+    }
+  }
   return (
     <div className="register-container">
       <div className="content">
@@ -22,14 +48,31 @@ export default function Register() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Nome da ONG" />
-          <input type="email" placeholder="E-mail" />
-          <input placeholder="Whatsap" />
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome da ONG"
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            placeholder="Whatsap"
+            onChange={(event) => setWhatsapp(event.target.value)}
+          />
 
           <div className="input-group">
-            <input placeholder="Cidade" />
-            <input placeholder="UF" style={{ width: 80 }} />
+            <input
+              placeholder="Cidade"
+              onChange={(event) => setCity(event.target.value)}
+            />
+            <input
+              placeholder="UF"
+              style={{ width: 80 }}
+              onChange={(event) => setUf(event.target.value)}
+            />
           </div>
 
           <button className="button" type="submit">
